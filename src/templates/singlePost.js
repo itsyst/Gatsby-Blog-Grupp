7efbe8eee0054021eebd2a6d6653b57581b665f9
Khale El Hamzi <Khaled.elhamzi.syne19lin@tucsweden.se>
2020-09-16 12:20:43 +0200

    added-query-single-post

--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -3,3 +3,39 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { H1 } from "../elements"
 import { Container, Post, FeatureImage } from "../components"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const pageQuery = graphql`
+query SinglePostQuery($id: String!) {
+    mdx(id: { eq: $id }) {
+    body
+    frontmatter {date excerpt slug title
+        featureImage {
+            publicURL
+            childImageSharp {
+                fixed {
+                ...GatsbyImageSharpFixed
+                }
+            }
+        }
+    }
+}
+`
