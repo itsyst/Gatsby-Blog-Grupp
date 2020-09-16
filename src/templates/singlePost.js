d2ee2ee0387fd1bfea09e7101ab0d4c4d68c1049
Khale El Hamzi <Khaled.elhamzi.syne19lin@tucsweden.se>
2020-09-16 12:22:33 +0200

    updated-single-post

--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -5,23 +5,22 @@ import { H1 } from "../elements"
 import { Container, Post, FeatureImage } from "../components"
 
 
+const singlePost = ({ data }) => {
+    const featureImage = data.mdx.frontmatter.featureImage.childImageSharp.fixed
 
+    return (
+        <Container>
+            <FeatureImage fixed={featureImage} />
+            <Post>
+                <H1 margin="0 0 2rem 0">{data.mdx.frontmatter.title}</H1>
+                <MDXRenderer>{data.mdx.body}</MDXRenderer>
+            </Post>
+        </Container>
+    )
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 export const pageQuery = graphql`
 query SinglePostQuery($id: String!) {
